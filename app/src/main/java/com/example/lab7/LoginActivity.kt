9fa4616a3dbf53295d72a136a6323d044a4a5986
Alish0404 <aabilakim17@gmail.com>
2025-03-30 09:11:49 +0600

    created LoginActivity

---
+++ app/src/main/java/com/example/lab7/LoginActivity.kt
@@ -0,0 +1,49 @@
+package com.example.lab7;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var edUsername: EditText
+    private lateinit var edPassword: EditText
+    private lateinit var btnLogin: Button
+    private lateinit var btnSignUp: Button
+    private val PREFS_NAME = "UserCredentials"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        edUsername = findViewById(R.id.ed_username)
+        edPassword = findViewById(R.id.ed_password)
+        btnLogin = findViewById(R.id.btn_login)
+        btnSignUp = findViewById(R.id.btn_signup)
+
+        // Переход к регистрации
+        btnSignUp.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
+
+        // Проверка логина/пароля
+        btnLogin.setOnClickListener {
+            val prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE)
+            val savedUser = prefs.getString("username", "")
+            val savedPass = prefs.getString("password", "")
+
+            val inputUser = edUsername.text.toString()
+            val inputPass = edPassword.text.toString()
+
+            if (inputUser == savedUser && inputPass == savedPass) {
+                Toast.makeText(this, "Вход выполнен!", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this, FileActivity::class.java))
+            } else {
+                Toast.makeText(this, "Неверные данные!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
